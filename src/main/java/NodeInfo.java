8bdf2d278155d66402f5c40a93a3659e85133f8d
ZubencoDmitri <mitribenzuben@yahoo.com>
2017-11-17 12:03:17 +0200

    initial commit

---
+++ src/main/java/NodeInfo.java
@@ -0,0 +1,17 @@
+public class NodeInfo {
+    private int port;
+    private int numberOfNodes;
+
+    public NodeInfo(int port, int numberOfNodes) {
+        this.port = port;
+        this.numberOfNodes = numberOfNodes;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public int getNumberOfNodes() {
+        return numberOfNodes;
+    }
+}
